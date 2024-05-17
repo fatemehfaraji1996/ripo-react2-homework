32ffebf36f5a2c3aec98b9c700f22ab4556daa1e
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-05-17 23:57:30 +0330

    jj

--- src/components/CustomerInformation.jsx
+++ src/components/CustomerInformation.jsx
@@ -4,6 +4,8 @@ function CustomerInformation({first_name,last_name}) {
   <h1>My name is {first_name}</h1>
   <h2>My last name is {last_name}</h2>
 </div>
+
+
     )
 }
 export default CustomerInformation
